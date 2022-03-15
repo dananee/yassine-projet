365978a69f7e7fd7a0e17a4a12fccad4c2c630a3
danane <dananeabdjalil2@gmail.com>
2022-03-15 13:20:53 +0100

    change color

--- lib/home.dart
+++ lib/home.dart
@@ -17,41 +17,44 @@ class Home extends StatelessWidget {
         title: const Text("App Name"),
         centerTitle: true,
       ),
-      body: ListView.builder(
+      body: ListView.separated(
           physics: const BouncingScrollPhysics(),
           itemCount: data.length,
+          separatorBuilder: (context, index) => const Divider(
+                thickness: 0.5,
+                color: Color.fromARGB(255, 197, 197, 197),
+              ),
           itemBuilder: (context, index) {
             final item = data[index];
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: item.icon,
-                    title: Text(
-                      item.title,
-                      style: const TextStyle(color: Colors.blue),
-                    ),
-                    trailing:
-                        const Icon(Icons.arrow_forward_ios_rounded, size: 16.0),
-                    onTap: () {
-                      g_ads.interInstance.showInterstitialAd();
-                      Get.to(() => Dettailes(),
-                          arguments: {
-                            "title": item.title,
-                            "desc": item.dec,
-                          },
-                          transition: Transition.rightToLeftWithFade);
-                    },
+            return Column(
+              children: [
+                ListTile(
+                  leading: item.icon,
+                  title: Text(
+                    item.title,
+                    style: const TextStyle(color: Colors.blue),
                   ),
-                  const Divider()
-                ],
-              ),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    size: 16.0,
+                    color: Color.fromARGB(255, 197, 197, 197),
+                  ),
+                  onTap: () {
+                    g_ads.interInstance.showInterstitialAd();
+                    Get.to(() => Dettailes(),
+                        arguments: {
+                          "title": item.title,
+                          "desc": item.dec,
+                        },
+                        transition: Transition.rightToLeftWithFade);
+                  },
+                ),
+              ],
             );
           }),
       bottomNavigationBar: SizedBox(
         height: 70,
-        child: CustomBanner(key: UniqueKey(), ads: g_ads.bannerInstance),
+        // child: CustomBanner(key: UniqueKey(), ads: g_ads.bannerInstance),
       ),
     );
   }
